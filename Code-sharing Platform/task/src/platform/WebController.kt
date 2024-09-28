6c5958cd0336acaebbe9571cd2851b380eb215e6
james <james1.6@hotmail.com>
2024-09-28 21:26:24 +0930

    Clean up commented-out HTML code in WebController

--- Code-sharing Platform/task/src/platform/WebController.kt
+++ Code-sharing Platform/task/src/platform/WebController.kt
@@ -38,41 +38,6 @@ class WebController(
     @GetMapping("/code/new")
     fun postNewCode(): ResponseEntity<String> {
         val responseText = resourceLoader.getResource("classpath:newCode.html").file.readText()
-//        val responseText =
-//            """
-//                <!DOCTYPE html>
-//                <html lang="en">
-//                <head>
-//                    <meta charset="UTF-8">
-//                    <title>Create</title>
-//                </head>
-//                <body>
-//                <h2>Update Code Snippit</h2>
-//                <textarea id="code_snippet"></textarea>
-//                <p>
-//                    <button id="send_snippet" type="submit" onclick="send()">Submit</button>
-//                </p>
-//                <script>
-//                function send() {
-//                    let object = {
-//                        "code": document.getElementById("code_snippet").value
-//                    };
-//
-//                    let json = JSON.stringify(object);
-//
-//                    let xhr = new XMLHttpRequest();
-//                    xhr.open("POST", '/api/code/new', false)
-//                    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-//                    xhr.send(json);
-//
-//                    if (xhr.status == 200) {
-//                        alert("Success!");
-//                    }
-//                }
-//                </script>
-//                </body>
-//                </html>
-//            """.trimIndent()
         return ResponseEntity.ok()
             .contentType(MediaType.TEXT_HTML)
             .body(responseText)
